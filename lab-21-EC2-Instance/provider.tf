43de61de1a40d8239ebeda62959eda1bab8b9439
mike-griffin1978 <41023207+mike-griffin1978@users.noreply.github.com>
2023-06-01 13:26:57 -0700

    Update provider.tf

--- lab-21-EC2-Instance/provider.tf
+++ lab-21-EC2-Instance/provider.tf
@@ -1,6 +1,6 @@
 # Provider Block
 provider "aws" {
-  profile = "srelab" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
 }
 
