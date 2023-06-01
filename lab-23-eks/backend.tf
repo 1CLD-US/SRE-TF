fce8780dfadbefce69ceb182dacdc89ccc1b1e8d
mike-griffin1978 <41023207+mike-griffin1978@users.noreply.github.com>
2023-06-01 13:43:20 -0700

    Update backend.tf

--- lab-23-eks/backend.tf
+++ lab-23-eks/backend.tf
@@ -1,7 +1,8 @@
+# Under key, update 'X' with student pod number
 terraform {
   backend "s3" {
     bucket         = "onecloud-kube"
-    key            = "pod1-kube.tfstate"
+    key            = "podx-kube.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
